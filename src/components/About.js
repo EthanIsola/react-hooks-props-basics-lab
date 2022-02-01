9fc578bcf8b879a470a36544d484dfe25c47451e
Ethan Isola <isolaethan@gmail.com>
2022-02-01 09:18:33 -0600

    commit tree 1beaa410d55f891fc520ee16086b0060eac57858

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {props.bio ? <p>{props.bio}</p> : ""}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" /> 
       {/* add your <Links /> component here */}
+      <Links github = {props.github} linkedin = {props.linkedin}/>
     </div>
   );
 }
 
+// function Links(){
+//   return(
+//     <div>
+//       <h3>Links</h3>
+//     </div>
+//   )
+// }
 export default About;
